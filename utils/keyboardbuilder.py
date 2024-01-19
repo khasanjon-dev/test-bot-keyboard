352756c5b1eb2e2f70256502534c5bb3494de229
Khasanjon <khasanjon.dev@gmail.com>
2024-01-19 10:23:10 +0500

    test create ready

--- utils/keyboardbuilder.py
+++ utils/keyboardbuilder.py
@@ -1,13 +1,22 @@
 from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+from data import data
 
-def keyboard_builder(messages: list, sizes: list | None = None):
+
+def keyboard_builder(messages: list, sizes: list | None = None, back=False, main=False):
     builder = ReplyKeyboardBuilder()
     for message in messages:
         builder.add(
             KeyboardButton(text=message)
         )
+    if back:
+        builder.add(
+            KeyboardButton(text=data.back_buttons['back'])
+        )
+    if main:
+        builder.add(
+            KeyboardButton(text=data.back_buttons['back_main_menu'])
+        )
     builder.adjust(*sizes)
     return builder.as_markup(resize_keyboard=True)
-
