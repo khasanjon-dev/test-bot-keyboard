edee70bb66747475a0442ece8316ba495f4886fc
Khasanjon <khasanjon.dev@gmail.com>
2024-01-19 10:23:11 +0500

    test create ready

--- logic/menu.py
+++ logic/menu.py
@@ -3,7 +3,6 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from data import data
-from logic.create_test import create_test_menu_handler
 from utils import states
 from utils.keyboardbuilder import keyboard_builder
 
@@ -12,14 +11,10 @@ menu = Router()
 
 @menu.message(states.Menu.main_menu)
 async def main_menu_handler(msg: Message, state: FSMContext):
-    if msg.text == data.main_menu['create_test']:
-        await state.set_state(states.CreateTest.menu)
-        await create_test_menu_handler(msg, state)
-    else:
-        markup = keyboard_builder(data.main_menu.values(), [1, 1, 2])
-        await msg.answer("Kerakli bo'limlardan birini tanlang!", reply_markup=markup)
-
-
-async def main_menu_keyboards(msg: Message, state: FSMContext):
     markup = keyboard_builder(data.main_menu.values(), [1, 1, 2])
-    await msg.answer(reply_markup=markup)
+    await msg.answer("Bo'limlardan birini tanlang!", reply_markup=markup)
+
+
+async def main_menu_keyboards(msg: Message):
+    markup = keyboard_builder(data.main_menu.values(), [1, 1, 2])
+    await msg.answer("Bo'limlardan birini tanlang!", reply_markup=markup)
